27e1996c63235bd678225eb2e19544f568bb0b55
ch <ch@mail.startek-eng.com>
2019-11-08 10:39:25 +0800

    More Express API setup
    sec. 8, video 111 of Docker: The complete guide
    
    Note that we are making a duplicate connection. According to the Redis
    documentation for this JavaScript library, if we ever have a client
    that's listening or publishing information on Redis, we have to make a
    duplicate connection because when a connection is turned into a
    connection, that's going to listen or subscribe or publish information,
    it can't be used for other purposes. That's why we are doing this
    duplicate thing here and other locations (in worker process).
    
    Note that when the route handler receives an index, it will set the hash
    of the index ('Nothing yet!') into Redis. Then it will publish a new
    'insert' event, which is the one the worker subscribes.
    The worker will calculate the Fib number of this index and replace the
    'Nothing yet!' string inside of Redis.

--- server/index.js
+++ server/index.js
@@ -23,3 +23,56 @@ pgClient.on('error', () => console.log('Lost PG connection'));
 pgClient
   .query('CREATE TABLE IF NOT EXISTS values (number INT)')
   .catch(err => console.log(err));
+
+// Redis Client Setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
+
+app.get('/', (req, res) => {
+  res.send('Hi');
+});
+
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * from values');
+
+  res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post('/values', async (req, res) => {
+  const index = req.body.index;
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send('Index too high');
+  }
+
+  redisClient.hset('values', index, 'Nothing yet!');
+  // Publish a new insert event of that index.
+  // Means sending the message over to the worker process.
+  // And the worker will come to the hash that we are
+  // creating inside of Redis and it's going to replace
+  // the string with the actual caculated value.
+  redisPublisher.publish('insert', index);
+
+  // Store the record of all the different indices that have
+  // been submitted in Postgres
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+  res.send({ working: true });
+});
+
+app.listen(5000, err => {
+  console.log('Listening');
+});
